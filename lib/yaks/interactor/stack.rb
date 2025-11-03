9a92d62956b4df041bbc70790bc5fe640a1f2ecf
Eugene Yak <geneayak@gmail.com>
2025-11-03 16:52:14 +0300

    Add Stack class for Yaks::Interactor

---
+++ lib/yaks/interactor/stack.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module Yaks
+  module Interactor
+    class Stack < Delegator
+      extend Fiber::Local
+
+      def initialize
+        super([])
+      end
+
+      attr_reader :stack
+
+      def __getobj__
+        @stack
+      end
+
+      def __setobj__(obj)
+        @stack = obj
+      end
+
+      def parent
+        stack[-2]
+      end
+
+      def root
+        stack.first
+      end
+
+      def root?
+        stack.count == 1
+      end
+    end
+  end
+end
